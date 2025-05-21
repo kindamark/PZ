42c6928e74e507f4113bd213ea0c4587227e4486
Mengze Wu <kindamark@gmail.com>
2025-05-21 17:24:39 +0800

    Show mouse cursor

--- PZ23.data.js
+++ PZ23.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/PZ23/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 81, "filename": "/UE4CommandLine.txt"}, {"start": 81, "audio": 0, "end": 26446582, "filename": "/PZ23/Content/Paks/PZ23-HTML5.pak"}], "remote_package_size": 26446582, "package_uuid": "f39e9e1c-2b9d-4349-997a-697a8958375d"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 81, "filename": "/UE4CommandLine.txt"}, {"start": 81, "audio": 0, "end": 26450582, "filename": "/PZ23/Content/Paks/PZ23-HTML5.pak"}], "remote_package_size": 26450582, "package_uuid": "76534dea-acf9-476e-a0c7-44155a6b95d1"});
 
 })();
